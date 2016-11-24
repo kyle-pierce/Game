6e948f9f1dba63471147ee4bd8290cd16f2beb3d
Kyle Pierce <pierce.kyle1996@gmail.com>
2016-11-23 20:37:21 -0800

    Update Player.java
    Significantly shortened the file by creating classes 'inventory' and 'equipment'.

--- Player.java
+++ Player.java
@@ -5,16 +5,14 @@ public class Player {
    private Scanner console = new Scanner(System.in);
    private String name;
    private String playerClass;
-   private Map<Item, Integer> inventory = new TreeMap<Item, Integer>();
-   private Map<String, Item> equipment = new TreeMap<String, Item>();
-   private int[] stats = new int[3]; //index 0 is strength, 1 is agility, 2 is megicka
+   private Inventory inventory;
+   private Equipment equipment;
 
    public Player() {
       makeName();
       makeRole();
-      makeInventory();
-      makeEquipment();
-      setStats();
+      inventory = new Inventory(playerClass);
+      equipment = new Equipment();
    }
    
    private void makeName() {
@@ -58,50 +56,6 @@ public class Player {
          return "mage";
       }
    } 
-      
-   private void makeInventory() {
-      if (playerClass.equals("warrior")) {
-         inventory.put(new Weapon("iron sword", 5, "main-hand"), 1);
-         inventory.put(new Weapon("hide buckler", 10, "off-hand"), 1);
-      } else if (playerClass.equals("ranger")) {
-         inventory.put(new Weapon("hunting bow", 5, "main-hand"), 1);
-         inventory.put(new Item("arrow"), 100);
-      } else {
-         inventory.put(new Weapon("gnarled staff", 5, "main-hand"), 1);
-         inventory.put(new Weapon("oak wand", 5, "main-hand"), 1);
-      }
-      inventory.put(new Item("gold coin"), 10);
-      inventory.put(new Item("lantern"), 1);
-      inventory.put(new Armor("Gold Chestpiece", 100, "chest"), 1);
-   }
-   
-   private void makeEquipment() {
-      equipment.put("head", null);
-      equipment.put("chest", null);
-      equipment.put("legs", null);
-      equipment.put("hands", null);
-      equipment.put("feet", null);
-      equipment.put("waist", null);
-      equipment.put("shoulders", null);
-      equipment.put("main-hand", null);
-      equipment.put("off-hand", null);
-   }
-   
-   private void setStats() {
-      if (playerClass.equals("warrior")) {
-         stats[0] = 10;
-         stats[1] = 0;
-         stats[2] = 0;
-      } else if (playerClass.equals("ranger")) {
-         stats[0] = 0;
-         stats[1] = 10;
-         stats[2] = 0;
-      } else {
-         stats[0] = 0;
-         stats[1] = 0;
-         stats[2] = 10;
-      }
-   }
    
    public String getRole() {
       return playerClass;
@@ -112,56 +66,14 @@ public class Player {
    }
    
    public void printInventory() {
-      System.out.println("Current Player Inventory:");
-      System.out.println();
-      for (Item i : inventory.keySet()) {
-         System.out.println(i + ": " + inventory.get(i));
-      }
-      System.out.println();
+      inventory.print();
    }
    
    public void printEquipment() {
-      System.out.println("Current Player Equipment:");
-      System.out.println();
-      for (String s : equipment.keySet()) {
-         if (equipment.get(s) != null) {
-            System.out.print(s + ": " + equipment.get(s));
-         }
-      }
-      if (equipment.values().size() == 1) {
-         System.out.println("You do not have anything equipped.");
-      }
-      System.out.println();
+      equipment.print();
    }
    
-   public void stats() {
-      System.out.println("Your current attributes are: ");
-      System.out.println(" Strength: " + stats[0]);
-      System.out.println(" Agility:  " + stats[1]);
-      System.out.println(" Magicka:  " + stats[2]);
-      System.out.println();
-   }
-   
-   public void equip(String itemName) {
-      Item equipped = checkInventory(itemName);
-      if (equipped != null) {
-         if (equipped.getSlot().length() > 0) {
-            equipment.put(equipped.getSlot(), equipped);
-            inventory.remove(equipped);
-         } else {
-            System.out.println("You can't equip that!");
-         }
-      } else {
-         System.out.println("Looks like you don't have any of those.");
-      }
-   }
-   
-   private Item checkInventory(String itemName) {
-      for (Item i : inventory.keySet()) {
-         if (i.toString().equals(itemName)) {
-            return i;
-         }
-      }
-      return null;
+   public void equip(String s) {
+      equipment.add(s, inventory);
    }
 }
