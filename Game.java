ce51149ab85eaa872dced883e8465df4ba9dcaee
Kyle Pierce <pierce.kyle1996@gmail.com>
2016-11-24 15:44:30 -0800

    Update Game.java
    Adds functionality to the help option.

--- Game.java
+++ Game.java
@@ -1,15 +1,16 @@
 import java.util.*;
+import java.io.*;
 
 public class Game {
-   private static String[] commands = {"show inventory","show equipment", "equip", "end"};
+   private static String[] commands = {"show inventory","show equipment", "equip", "help", "end"};
    private static Scanner console = new Scanner(System.in);
    private static Player currentPlayer;
 
-   public static void main(String[] args) {
+   public static void main(String[] args) throws FileNotFoundException {
       currentPlayer = new Player();
       String input = "";
       while (!input.equals("end")) {
-         System.out.print("Awaiting input: ");
+         System.out.print("Awaiting input ('help' for more info): ");
          input = console.nextLine().toLowerCase().trim();
          System.out.println();
          determineAction(processInput(input));
@@ -28,13 +29,19 @@ public class Game {
       }
    }
    
-   public static void determineAction(String command) {
+   public static void determineAction(String command) throws FileNotFoundException {
       if (command.equals("show inventory")) {
          currentPlayer.printInventory();
       } else if (command.equals("show equipment")) {
          currentPlayer.printEquipment();
       } else if (command.startsWith("equip")) {
          currentPlayer.equip(command.substring(5).trim());
+      } else if (command.equals("help")) {
+         Scanner text = new Scanner(new File("HelpCommands.txt"));
+         while (text.hasNext()) {
+            System.out.println(text.nextLine());
+         }
+         System.out.println();
       }
    }
 }
